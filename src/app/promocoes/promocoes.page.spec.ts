975b96b21987c9e346bb112fbc9a8fd3e8d52296
MatsudaEol <110265861+MatsudaEol@users.noreply.github.com>
2023-08-24 19:54:16 -0300

    Aula 6
    Criação dos "Serviços", com ênfase no "carrinho"

--- src/app/promocoes/promocoes.page.spec.ts
+++ src/app/promocoes/promocoes.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { PromocoesPage } from './promocoes.page';
 
 describe('PromocoesPage', () => {
@@ -11,7 +11,7 @@ describe('PromocoesPage', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('should create', () => { 
     expect(component).toBeTruthy();
   });
 });
